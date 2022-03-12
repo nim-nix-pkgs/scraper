06720ae57b2d251c5d5db08f8505208f718b40a9
hugosenari <hugosenari@gmail.com>
2022-03-12 03:59:02 -0300

    chore: re index scraper

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Scraping tools'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-scraper-master.flake = false;
+  inputs.src-scraper-master.owner = "lurlo";
+  inputs.src-scraper-master.ref   = "refs/heads/master";
+  inputs.src-scraper-master.repo  = "scraper";
+  inputs.src-scraper-master.type  = "gitlab";
+  
+  inputs."https://github.com/thisago/findxml".dir   = "nimpkgs/h/https://github.com/thisago/findxml";
+  inputs."https://github.com/thisago/findxml".owner = "riinr";
+  inputs."https://github.com/thisago/findxml".ref   = "flake-pinning";
+  inputs."https://github.com/thisago/findxml".repo  = "flake-nimble";
+  inputs."https://github.com/thisago/findxml".type  = "github";
+  inputs."https://github.com/thisago/findxml".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."https://github.com/thisago/findxml".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-scraper-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-scraper-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
